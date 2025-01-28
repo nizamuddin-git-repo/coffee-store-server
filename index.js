392b284a8cc38efe79edfa11471da64368160605
nizamuddin0172 <neom01723441081@gmail.com>
2025-01-28 12:52:54 +0600

    add user

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ async function run() {
     await client.connect();
 
     const coffeeCollection = client.db('coffeeDB').collection('coffee');
+    const userCollection =client.db('coffeeDB').collection('user')
 
     app.get('/coffee', async(req,res)=>{
       const cursor = coffeeCollection.find();
@@ -70,13 +71,34 @@ async function run() {
         res.send(result)
     })
 
-
     app.delete('/coffee/:id', async(req,res)=>{
       const id = req.params.id;
       const query = {_id: new ObjectId(id)}
       const result = await coffeeCollection.deleteOne(query);
       res.send(result);
     })
+
+    // user related apis
+    app.get('/user', async(req,res)=>{
+      const cursor = userCollection.find();
+      const users = await cursor.toArray();
+      res.send(users)
+    })
+
+    app.post('/user', async(req,res)=>{
+      const user = req.body;
+      console.log(user);
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
+    app.delete('/user/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query)
+      res.send(result)
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
